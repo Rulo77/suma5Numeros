a63fab14502f873b7198a55a6b8d67617b2c6a1f
Raul Gomez <ragomez1234567@gmail.com>
2021-10-21 17:05:00 -0600

    Add project files.

---
+++ suma de 5 numeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace suma_de_5_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int n;
+            int acu = 0;
+            while (num <= 5)
+            {
+                Console.WriteLine("introduce el " + num + " numero");
+                n = int.Parse(Console.ReadLine());
+                acu = acu + n;
+
+                num++;
+            }
+            Console.WriteLine("la suma de los 5 numeros es: " + acu);
+            Console.ReadKey();
+        }
+    }
+    
+}
